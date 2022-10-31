335c3fa963f452c2d1c0dd21fce67e97f87b9691
Matt Walter <matt.walter.tx@outlook.com>
2022-10-31 17:52:16 -0500

    Refactor RecipeRepository.js

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -2,6 +2,7 @@ class RecipeRepository {
   constructor(recipes) {
     this.recipes = recipes
   }
+
   filterByTag = (tag) => {
     const filteredList = this.recipes.filter(recipe => {
       let tags = Object.values(recipe.tags)
@@ -11,14 +12,8 @@ class RecipeRepository {
     })
     return filteredList
   }
+  
   filterByName = (name) => {
-    // Iterate through the list of recipes using .filter()
-    // Access each recipe object's name string
-    // Make the name string lowercase with .toLowerCase()
-    // Use .include() on each name to see if any part
-    // of its name matches the argument
-    // return matched ones to a new array
-    // return the array in the outer scope
     const filteredList = this.recipes.filter(recipe => {
       let lowerCaseRecipeName = recipe.name.toLowerCase()
       if(lowerCaseRecipeName.includes(name.toLowerCase())) {
