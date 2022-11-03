66e36f698c504d77dbc1ace4e2f6ab07765b0bbc
Matt Walter <matt.walter.tx@outlook.com>
2022-11-03 07:49:06 -0500

    Refactor: altered testing path to deep equal

--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,42 +1,45 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
-const data = require('../src/data/RecipeRepository-test-data.js')
+const data = require('../src/data/RecipeRepository-test-data.js');
 
 describe('RecipeRepository', () => {
+  let recipesList, recipeRepo;
 
   beforeEach(() => {
-    let recipesList = data.recipesList
-  })
+    recipesList = data.recipesList;
+    recipeRepo = new RecipeRepository(recipesList);
+  });
 
   it('Should be a function', () => {
-    expect(RecipeRepository).to.be.a('function')
-  })
-  it('Should store recipes', () => {
-    const recipeRepo = new RecipeRepository(recipesList)
-    expect(recipeRepo.recipes).to.be.an('array')
-  })
+    expect(RecipeRepository).to.be.a('function');
+  });
+
+  it('Should be able to store recipes', () => {
+    expect(recipeRepo.recipes).to.be.an('array');
+  });
+
   it('Should be able to filter recipes by tag', () => {
-    const recipeRepo = new RecipeRepository(recipesList)
-    expect(recipeRepo.filterByTag('side dish').length).to.equal(2)
-    expect(recipeRepo.filterByTag('snack').length).to.equal(1)
-  })
+    expect(recipeRepo.filterByTag('side dish')).to.deep.equal([data.recipesList[2], data.recipesList[3]]);
+    expect(recipeRepo.filterByTag('snack')).to.deep.equal([recipesList[0]]);
+  });
+
   it('Should be able to filter recipes by name', () => {
-    const recipeRepo = new RecipeRepository(recipesList)
-    expect(recipeRepo.filterByName('sauce').length).to.equal(1)
-    expect(recipeRepo.filterByName('pork chop').length).to.equal(1)
-  })
+    expect(recipeRepo.filterByName('sauce')).to.deep.equal([recipesList[2]]);
+    expect(recipeRepo.filterByName('pork chop')).to.deep.equal([recipesList[1]]);
+  });
+
   it('Should not be case-sensitive when filtering recipes by name', () => {
-    const recipeRepo = new RecipeRepository(recipesList)
-    expect(recipeRepo.filterByName('SaUCe').length).to.equal(1)
-    expect(recipeRepo.filterByName('PORK chop').length).to.equal(1)
-  })
+    expect(recipeRepo.filterByName('SaUCe')).to.deep.equal([recipesList[2]]);
+    expect(recipeRepo.filterByName('PORK chop')).to.deep.equal([recipesList[1]]);
+  });
+
   it('Should not return a recipe that does not exist', () => {
-    const recipeRepo = new RecipeRepository(recipesList)
-    expect(recipeRepo.filterByName('taco').length).to.equal(0)
-    expect(recipeRepo.filterByName('lemon cake').length).to.equal(0)
-  })
+    expect(recipeRepo.filterByName('taco')).to.deep.equal([]);
+    expect(recipeRepo.filterByName('lemon cake')).to.deep.equal([]);
+  });
+
   it('Should not return any recipe if the search is blank', () => {
-    const recipeRepo = new RecipeRepository(recipesList)
-    expect(recipeRepo.filterByName('').length).to.equal(0)
-  })
+    expect(recipeRepo.filterByName('')).to.deep.equal([]);
+  });
+
 })
