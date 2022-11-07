49e7d58aa2d26ceaec4044a0f3793b43f9cbf803
Evan Swanson <evan.sundelius@gmail.com>
2022-11-06 20:24:49 -0600

    Improve tabability

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
+//IMPORTS
 import './styles.css';
 import apiCalls from './apiCalls';
 import gatherData from './apiCalls';
@@ -7,7 +8,7 @@ import Recipe from './classes/Recipe.js';
 import RecipeRepository from './classes/RecipeRepository.js';
 import User from './classes/User.js';
 
-// Declare variables for linked methods compatibility
+//GLOBAL VARIABLES
 let usersData;
 let ingredientsData;
 let recipeData;
@@ -17,11 +18,7 @@ let recipeCards;
 let currentRecipe;
 let pantryUpdateArea;
 
-function makeAllHidden() {
-  recipeGrid.classList.toggle('hidden');
-}
-
-// Declare function to instantiate all of our data to dashboard on load/ refresh.
+//API CALLS
 function instantiateData() {
   Promise.all([
     gatherData('http://localhost:3001/api/v1/users'),
@@ -49,24 +46,10 @@ function modifyUserData(userId, ingredient) {
   })
   .then(response => response.json())
   .then(reloadUserDashboard())
-  .catch(err => console.log(err))
-  //we'll have to call a user method here to reflect our changes to the user class's pantry
-}
+  .catch(err => console.log(err));
+};
 
-function reloadUserDashboard() {
-  recipeCards = recipeData.map(recipe => {
-    const newCard = new Recipe (recipe);
-    return newCard
-  });
-  newRecipeRepo = new RecipeRepository (recipeCards);
-  renderUser(currentUser);
-  renderAllRecipes(recipeCards);
-  renderPantry()
-  renderFavoriteRecipes(currentUser.recipesToCook)
-}
-
-
-// Query Selectors
+//QUERY SELECTORS
 const allRecipesGrid = document.querySelector('#all-card-grid');
 const favoriteRecipesGrid = document.querySelector('#fave-card-grid');
 const greeting = document.querySelector('#greeting');
@@ -83,9 +66,8 @@ const homeButton = document.querySelector('#buttonOfHome');
 const pantryButton = document.querySelector('#pantry-button');
 const pantryList = document.querySelector('#pantry-list');
 
-// Event Listeners
-window.addEventListener('load', instantiateData());
-allRecipesGrid.addEventListener('click', showRecipe);
+//GLOBAL EVENT LISTENERS
+window.addEventListener('load', instantiateData);
 mainSearchBar.addEventListener('keyup', filterRecipe);
 favoritedSearchBar.addEventListener('keyup', searchFavoritedRecipes)
 favoritesNavButton.addEventListener('click', viewFavoriteRecipes)
@@ -93,101 +75,114 @@ favoriteButton.addEventListener('click', addToFavorites);
 homeButton.addEventListener('click', showAllRecipes);
 pantryButton.addEventListener('click', showPantry);
 
-
-
-
-// Functions
+//FUNCTIONS
 function loadUser() {
   currentUser = new User(
-
-    usersData[Math.floor(Math.random() * usersData.length)]
+  usersData[Math.floor(Math.random() * usersData.length)]
   );
   recipeCards = recipeData.map(recipe => {
     const newCard = new Recipe (recipe);
-    return newCard
+    return newCard;
   });
   newRecipeRepo = new RecipeRepository (recipeCards);
   renderUser(currentUser);
   renderAllRecipes(recipeCards);
-  renderPantry()
-}
+  renderPantry();
+};
+
+function reloadUserDashboard() {
+  recipeCards = recipeData.map(recipe => {
+    const newCard = new Recipe (recipe);
+    return newCard;
+  });
+  newRecipeRepo = new RecipeRepository (recipeCards);
+  renderUser(currentUser);
+  renderAllRecipes(recipeCards);
+  renderPantry();
+  renderFavoriteRecipes(currentUser.recipesToCook);
+};
 
 function renderUser(user) {
   greeting.innerHTML = '';
   greeting.innerHTML = `<h1 class="personalized-greeting"> Welcome to What\'s Cookin\',<br>${user.name}!</h1>`;
-}
+};
 
 function renderAllRecipes(data) {
   allRecipesGrid.innerHTML = '';
   allRecipesGrid.innerHTML = 
     data.map(recipe => `<li class="recipe-card">
-      <h3 class="" id="recipe-title">${recipe.name}</h3>
-      <img class="recipe-image-all" id="${recipe.id}" src="${recipe.image}" alt="${recipe.name}">
+      <h3 class="recipe-title-all" id="recipe-title">${recipe.name}</h3>
+      <button class="image-button" id="${recipe.id}"><img class="recipe-image-all" id="${recipe.id}" src="${recipe.image}" alt="${recipe.name}"></button>
       <h3 class="recipe-tags-all">
         ${recipe.tags}
       </h3>
     </li>`).join('');
-}
+  const imageButton = document.querySelectorAll('.image-button');
+  imageButton.forEach(image => {
+    image.addEventListener('click', showRecipe);
+  });
+};
 
 function renderFavoriteRecipes(data) {
   favoriteRecipesGrid.innerHTML = '';
   favoriteRecipesGrid.innerHTML = 
     data.map(recipe => `<li class="recipe-card">
     <h3 class="recipe-title-favorited" id="recipe-title">${recipe.name}</h3>
-    <img class="recipe-image-all" id="${recipe.id}" src="${recipe.image}">
+    <button class="image-button" id="${recipe.id}"><img class="recipe-image-all" id="${recipe.id}" src="${recipe.image}"></button>
     <h3 class="recipe-tags-all">
         ${recipe.tags}
       </h3>
     <button class="remove-button" id="${recipe.id}">Remove from Favorites</button>
   </li>`).join('');
-  const favoriteRecipeImagesAll = document.querySelectorAll('.recipe-image-all')
-  favoriteRecipeImagesAll.forEach(image => {
-    image.addEventListener('click', showRecipe)
-  })
+  const imageButton = document.querySelectorAll('.image-button');
   const removeFromFavoritesButton = document.querySelectorAll('.remove-button');
+  imageButton.forEach(image => {
+    image.addEventListener('click', showRecipe);
+  });
   removeFromFavoritesButton.forEach(button => {
     button.addEventListener('click', removeFromFavorites);
-  })
-}
+  });
+};
 
 function removeFromFavorites(event) {
   let recipeToRemove = currentUser.recipesToCook.findIndex(recipe => {
-    return recipe.id === parseInt(event.target.id)
-  })
-  currentUser.recipesToCook.splice(recipeToRemove, 1)
-  renderFavoriteRecipes(currentUser.recipesToCook)
-}
+    return recipe.id === parseInt(event.target.id);
+  });
+  currentUser.recipesToCook.splice(recipeToRemove, 1);
+  renderFavoriteRecipes(currentUser.recipesToCook);
+};
 
 function renderPantry() {
   const render = currentUser.pantry.map(ing => {
     let newIng = ingredientsData.find(i => i.id === ing.ingredient);
     let recMatch = recipeData.find(recipe => recipe.ingredients.find(z => z.id === ing.ingredient));
-    let newUnit = recMatch.ingredients.find(ingred => ingred.id === ing.ingredient)
+    let newUnit = recMatch.ingredients.find(ingred => ingred.id === ing.ingredient);
 
     const newObj = {
       name: (newIng && newIng.name) || "Undefined",
-      //should that be lowercase "undefined"? not sure if that makes a diff.
       amount: ing.amount,
       units: newUnit.quantity.unit
+    };
+    if(newObj.amount !== 0) {
+      return `<ul>${newObj.name}: ${newObj.amount} ${newObj.units}</ul>`;
     }
-    return `<ul>${newObj.name}: ${newObj.amount} ${newObj.units}</ul>`
   });
   pantryList.innerHTML = '';
   pantryList.innerHTML = 
     `${render.join('')}`
-}
+};
 
 function filterRecipe() {
   const recipeSearch = mainSearchBar.value;
   let filteredRecipesByName = newRecipeRepo.filterByName(recipeSearch);
-  let filteredRecipesByNameAndTag = filteredRecipesByName.concat(newRecipeRepo.filterByTag(recipeSearch))
+  let filteredRecipesByNameAndTag = filteredRecipesByName.concat(newRecipeRepo.filterByTag(recipeSearch));
   renderAllRecipes(filteredRecipesByNameAndTag);
 }
 
 function searchFavoritedRecipes() {
   const recipeSearch = favoritedSearchBar.value;
   let filteredRecipesByName = currentUser.filterByName(recipeSearch);
-  let filteredRecipesByNameAndTag = filteredRecipesByName.concat(currentUser.filterByTag(recipeSearch))
+  let filteredRecipesByNameAndTag = filteredRecipesByName.concat(currentUser.filterByTag(recipeSearch));
   renderFavoriteRecipes(filteredRecipesByNameAndTag);
 }
 
@@ -198,11 +193,11 @@ function showRecipe(event) {
   window.scrollTo(0, 0);
 
   const recipe = newRecipeRepo.recipes.find(recipe => {
-    return recipe.id === parseInt(event.target.id)
+    return recipe.id === parseInt(event.target.id);
   });
 
   currentRecipe = newRecipeRepo.recipes.find(recipe => {
-    return recipe.id === parseInt(event.target.id)
+    return recipe.id === parseInt(event.target.id);
   });
 
   const ingredients = recipe.ingredients.map(ing => {
@@ -218,7 +213,7 @@ function showRecipe(event) {
     `<div class="top-section-container">
     <img class="single-recipe-image" src="${recipe.image}" alt="${recipe.name}"></img>
       <div class="top-right-mini-container">
-      <button class="cook-button" id="${recipe.id}">Let's Cook!</button>
+        <button class="cook-button" id="${recipe.id}">Let's Cook!</button>
         <button class="add-missing-button" id="${recipe.id}">Add Missing Ingredients to Pantry!</button>
         <div class="pantry-update-area">
         </div>
@@ -233,8 +228,6 @@ function showRecipe(event) {
         <div>Total Cost</div>
         $${recipe.returnIngredientCost(ingredientsData)}
         <br>
-        <button class="cook-button" id="${recipe.id}">Let's Cook!</button>
-        <button class="add-missing-button" id="${recipe.id}">Add Missing Ingredients to Pantry!</button>
       </section>
       <section> 
         <div>Instructions</div>
@@ -248,18 +241,18 @@ function showRecipe(event) {
   favoriteButton.addEventListener('click', addToFavorites);
   cookButton.addEventListener('click', letsCook);
   addIngsButton.addEventListener('click', addIngredients);
-}
+};
 
 function addToFavorites(event) {
   let favoritedRecipe = newRecipeRepo.recipes.find(recipe => {
     if (recipe.id === parseInt(event.target.id)) {
-      return recipe
-    }
-  })
+      return recipe;
+    };
+  });
   if (!currentUser.recipesToCook.includes(favoritedRecipe)) {
     currentUser.addToCookList(favoritedRecipe)
-  }
-}
+  };
+};
 
 function letsCook() {
   const pantryStatus = currentUser.checkPantry(currentRecipe);
@@ -267,11 +260,11 @@ function letsCook() {
   if(insufficientArray.length === 0) {
     currentUser.removeFromPantry(currentRecipe);
     pantryUpdateArea.innerHTML = '';
-    pantryUpdateArea.innerHTML = `<h3 class="pantry-update-info">Recipe cooked! Ingredients have been removed from your pantry.</h3>`
+    pantryUpdateArea.innerHTML = `<h3 class="pantry-update-info">Recipe cooked! Ingredients have been removed from your pantry.</h3>`;
   }
   else {
     pantryUpdateArea.innerHTML = '';
-    pantryUpdateArea.innerHTML = `<h3 class="pantry-update-info">${displayMissingIngredients(pantryStatus)}</h3>`
+    pantryUpdateArea.innerHTML = `<h3 class="pantry-update-info">${displayMissingIngredients(pantryStatus)}</h3>`;
   };
 };
 
@@ -280,11 +273,11 @@ function displayMissingIngredients(pantryStatus) {
   pantryStatus.forEach(obj => {
     const correctIng = ingredientsData.find(ing => ing.id === obj.id);
     if(obj.stockStatus === 'not enough' || obj.stockStatus === 'empty') {
-      missingIngs.push(` ${(obj.recipeQ - obj.pantryQ)} ${obj.unit} of ${correctIng.name}`)
-    }
-  })
-    missingIngs[missingIngs.length - 1] = ` and ${missingIngs[missingIngs.length - 1]}`
-    return `You are missing: ${missingIngs}. Add them to your pantry to cook the recipe!`
+      missingIngs.push(` ${(obj.recipeQ - obj.pantryQ)} ${obj.unit} of ${correctIng.name}`);
+    };
+  });
+    missingIngs[missingIngs.length - 1] = ` and ${missingIngs[missingIngs.length - 1]}`;
+    return `You are missing: ${missingIngs}. Add them to your pantry to cook the recipe!`;
 };
 
 function addIngredients() {
@@ -299,7 +292,7 @@ function addIngredients() {
     pantryUpdateArea.innerHTML = '';
     pantryUpdateArea.innerHTML = `<h3 class="pantry-update-info">Ingredients added!</h3>`;
   };
-}
+};
 
 function viewFavoriteRecipes() {
   greeting.classList.add('hidden');
@@ -309,17 +302,8 @@ function viewFavoriteRecipes() {
   pantryView.classList.add('hidden');
   savedRecipesView.classList.remove('hidden');
   favoritedSearchBar.classList.remove('hidden');
-  renderFavoriteRecipes(currentUser.recipesToCook)
-}
-
-// As a user, I should be able to filter recipes by a tag. (Extension option: by multiple tags)
-// render page view/ unhide form of grid containing all recipe card objects WITH SELECTED TAG for All Recipes Page display (may as well reuse code for render?)
-// invoke with searchbar handler? Maybe easier to set a static list of all tag names to use as preset 'filters'? probably not.
-// invoke on recipe page with handler on click of tag name
-
-
-// As a user, I should be able to search recipes by their name. (Extension option: by name or ingredients)
-// searchbar should have a handler to search all recipes and filter by entered/ selected name OR tag
+  renderFavoriteRecipes(currentUser.recipesToCook);
+};
 
 function showAllRecipes() {
   greeting.classList.remove('hidden');
@@ -331,7 +315,7 @@ function showAllRecipes() {
   mainSearchBar.classList.remove('hidden')
   favoritedSearchBar.classList.add('hidden');
   renderAllRecipes(recipeData);
-}
+};
 
 function showPantry() {
   savedRecipesGrid.classList.add('hidden');
@@ -340,7 +324,7 @@ function showPantry() {
   favoritedSearchBar.classList.add('hidden');
   mainSearchBar.classList.remove('hidden')
   pantryView.classList.remove('hidden');
-  renderPantry()
-}
+  renderPantry();
+};
 
 export { modifyUserData };
