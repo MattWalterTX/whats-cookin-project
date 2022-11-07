239a25eb31f3a1c5aa6a9fcc11faa9dc61c7f309
Dana Truong <tramtramphotos@gmail.com>
2022-11-07 17:15:53 -0600

    Refactor Ingredient class test

--- test/Ingredient-test.js
+++ test/Ingredient-test.js
@@ -3,28 +3,32 @@ import Ingredient from '../src/classes/Ingredient';
 
 describe('Ingredient', () => {
 
-  let ingredientItem
-  let ingredient1
+  let ingredientItem;
+  let ingredient1;
 
   beforeEach(() => {
     ingredientItem = {
       "id": 19304,
       "name": "unsulfured molasses",
       "estimatedCostInCents": 925
-    }
-    ingredient1 = new Ingredient(ingredientItem)
-  })
+    };
+    ingredient1 = new Ingredient(ingredientItem);
+  });
 
   it('Should be a function', () => {
-    expect(Ingredient).to.be.a('function')
-  })
+    expect(Ingredient).to.be.a('function');
+  });
+
   it('Should store an ID', () => {
-    expect(ingredient1.id).to.equal(19304)
-  })
+    expect(ingredient1.id).to.equal(19304);
+  });
+
   it('Should store a name', () => {
-    expect(ingredient1.name).to.equal('unsulfured molasses')
-  })
+    expect(ingredient1.name).to.equal('unsulfured molasses');
+  });
+
   it('Should store an estimated cost in cents', () => {
-    expect(ingredient1.estimatedCostInCents).to.equal(925)
-  })
-})
+    expect(ingredient1.estimatedCostInCents).to.equal(925);
+  });
+
+});
